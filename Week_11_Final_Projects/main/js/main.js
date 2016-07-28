03020d5a54a593854a928285175263ca17bcd41a
andyaro3 <andyaro3@aol.com>
2016-07-28 14:13:21 -0400

    07/28/2016
    drafting portfolio page of final project

---
+++ Week_11_Final_Projects/main/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+// Project Name: Final Project, Main
+// Client: FEWD25
+// Author: Andy Aronoff
+// Developer @GA in DC
+
+
+
+
+
+
+
+
+}); //close document.ready
