52f3a0d19946f3499afbd5d4679888d8cffc6726
andyaro3 <andyaro3@aol.com>
2016-08-04 17:09:07 -0400

    08/04/2016
    friday changes

--- Week_11_Final_Projects/main/js/main.js
+++ Week_11_Final_Projects/main/js/main.js
@@ -5,24 +5,51 @@ $(document).ready(function() {
 // Author: Andy Aronoff
 // Developer @GA in DC
 
-// anchor animate
+// anchor animate, showCode slideToggle
 var hrefFull
 var hrefName
+var hrefCode = 0
+
 $("nav ul li a").click(function() {
   event.preventDefault();
   hrefFull = event.target.href;
   console.log(hrefFull);
   hrefName = hrefFull.substr(hrefFull.indexOf("#") + 0);
   console.log(hrefName);
-  // $("html, body").delay(10).animate({
-  //     scrollTop: $(hrefName).offset().top 
-  //   }, 1000);
+
+  // #showCode slideToggle
+  // if (hrefName == "#showCode") {
+  //   $("#showCode").slideToggle(400);
+  if (hrefName == "#showCode" && hrefCode == 0) {
+    $("#showCode").slideDown(400);
+    hrefCode = 1;
+  } else if (hrefName == "#showCode" && hrefCode == 1) {
+    $("#showCode").slideUp(400);
+    hrefCode = 0;
+  // scroll
+  } else {
   $("html, body").animate({
       scrollTop: $(hrefName).offset().top 
     }, 1000);
-});
+  };
+}); // end anchor animate, showCode slideToggle
 
-// call Unite Gallery
-$("#gallery").unitegallery();
+// hide #showCode div on window resize if showing
+$(window).resize(function() {
+  // This will fire each time the window is resized:
+  if ($(window).width() >= 760 && hrefCode == 0) {
+    // if large enough and not opened
+    $("showCode").hide();
+    hrefCode = 0;
+  } else if ($(window).width() >= 760 && hrefCode == 1) {
+    // if large enough window and opened
+    $("showCode").show();
+    hrefCode = 1;
+  } else {
+    // if smaller
+    $("#showCode").hide();
+    hrefCode = 0;
+  };
+});
 
 }); //close document.ready
