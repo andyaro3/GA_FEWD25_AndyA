c669a9440ad8e2f3f336f08a29b79a07a6c2acf3
andyaro3 <andyaro3@aol.com>
2016-08-09 14:19:09 -0400

    08/09/2016
    Week 8 Homework - Relaxr mobile

--- Week_11_Final_Projects/main/js/main.js
+++ Week_11_Final_Projects/main/js/main.js
@@ -5,11 +5,23 @@ $(document).ready(function() {
 // Author: Andy Aronoff
 // Developer @GA in DC
 
+// scrollBottom ftn, http://stackoverflow.com/questions/4655273/jquery-window-scrolltop-but-no-window-scrollbottom
+$.fn.scrollBottom = function(scroll){
+  if(typeof scroll === 'number'){
+    window.scrollTo(0,$(document).height() - $(window).height() - scroll);
+    return $(document).height() - $(window).height() - scroll;
+  } else {
+    return $(document).height() - $(window).height() - $(window).scrollTop();
+  }
+};
+
 // anchor animate, showCode slideToggle
 var hrefFull;
 var hrefName;
 var hrefCode = 0;
-var pageCode =  document.getElementsByTagName("body").text;
+var htmlPageCode =  $("html").html();
+var cssPageCode =  $("css").html();
+var jsPageCode =  $("js").html();
 
 $("nav ul li a").click(function() {
   event.preventDefault();
@@ -22,16 +34,28 @@ $("nav ul li a").click(function() {
   if (hrefName == "#showCode" && hrefCode == 0) {
     $("#showCode").slideDown(400);
     hrefCode = 1;
-    console.log(pageCode);
-    $("#showCode").text(pageCode);
+    console.log(htmlPageCode);
+    $("#htmlCode").text(htmlPageCode);
+    console.log(cssPageCode);
+    $("#cssCode").text(cssPageCode);
+    console.log(jsPageCode);
+    $("#jsCode").text(jsPageCode);
   } else if (hrefName == "#showCode" && hrefCode == 1) {
     $("#showCode").slideUp(400);
     hrefCode = 0;
-  // scroll
+  // scroll to bottom
+  } else if (hrefName == "#contactLink") {
+    //attempt 1 - scrolls past bottom of document, takes time to scroll back in
+    $("html, body").animate({
+        scrollTop: document.body.scrollHeight
+      }, 2000);
+    // scrollHeight = 0; // document.body.scrollHeight;
+    // console.log(scrollHeight);
+  // all other scroll
   } else {
-  $("html, body").animate({
-      scrollTop: $(hrefName).offset().top 
-    }, 1000);
+    $("html, body").animate({
+        scrollTop: $(hrefName).offset().top 
+      }, 1000);
   };
 }); // end anchor animate, showCode slideToggle
 
